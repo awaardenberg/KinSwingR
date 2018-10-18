a914d9721c1b0e87ee29a9403ce28c739f99ee44
Ashley J. Waardenberg <a.waardenberg@gmail.com>
2018-10-18 15:24:09 +1000

    update swingScore update to documentation added return_network option

--- R/swing.r
+++ R/swing.r
@@ -14,8 +14,10 @@
 #' @param pwm_in List of PWMs created using buildPWM()
 #' @param pwm_scores List of PWM-substrate scores created using
 #' scoreSequences()
-#' @param pseudo_count Pseudo-count for avoiding log-zero transformations.
-#' Default: "1"
+#' @param pseudo_count Pseudo-count acts at two levels. 1) It adds a small
+#' number to the counts to avoid zero divisions, which also 2) avoids log-zero 
+#' transformations. Note that this means that pos, neg and all values in the
+#' output table include the addition of the pseudo-count. Default: "1"
 #' @param p_cut_pwm Significance level for determining a significant
 #' kinase-substrate enrichment. Default: "0.05"
 #' @param p_cut_fc Significance level for determining a significant level of
@@ -24,6 +26,8 @@
 #' kinase-subtrate edges of the network n times. To not perform permutations and
 #'  only generate the scores, set permutations=1 or permutations=FALSE. Default:
 #'   "1000"
+#' @param return_network Option to return an interaction network for visualising
+#' in cystoscape. Default = FALSE
 #' @param verbose Turn verbosity on/off. To turn on, verbose=TRUE. Options are: 
 #' "TRUE, FALSE". Default=FALSE
 #'
@@ -45,13 +49,13 @@
 #'
 #' ## score the PWM - substrate matches
 #' ## Using a "random" background, to calculate the p-value of the matches
-#' ## Using n = 10 for demonstration
+#' ## Using n = 100 for demonstration
 #' ## set.seed for reproducibility
 #' set.seed(1234)
 #' substrate_scores <- scoreSequences(input_data = annotated_data,
 #'                                    pwm_in = pwms,
 #'                                    background = "random",
-#'                                    n = 10)
+#'                                    n = 100)
 #'
 #' ## Use substrate_scores and annotated_data data to predict kinase activity.
 #' ## This will permute the network node and edges 10 times for demonstration.
@@ -79,6 +83,7 @@ swing <-
            p_cut_pwm = 0.05,
            p_cut_fc = 0.05,
            permutations = 1000,
+           return_network = FALSE,
            verbose = FALSE) {
     #----------------------------------------------
     #format checks:
@@ -161,13 +166,14 @@ swing <-
       if (verbose) {
         message("[Step2/3] : Permuting Network")
       }
+      
+      # obtain permuted column names
+      swing_names <- colnames(data_merge)[7:ncol(data_merge)]
       n_permute <- lapply(seq_len(permutations), function(i)
-        sample(as.character(colnames(data_merge))[7:ncol(data_merge)],
-               length(colnames(data_merge[, 7:ncol(data_merge)])),
-               replace = FALSE))
-      # not really required
+        sample(as.character(swing_names), length(swing_names),replace = FALSE))
       names(n_permute) <- paste("rand", seq_len(permutations), sep = "")
-      # calculate swing scores and return in a single table
+      
+      # calculate swing scores (with permuted names) and return as a vector
       swing_permute <- list(bplapply(seq_len(permutations), function(i)
         swingScore(
           data_merge = setNames(data.frame(data_merge),
@@ -179,14 +185,16 @@ swing <-
           n = i
         )))
       
-      # uses data.table library to merge:
-      swing_permute <-
-        data.frame(Reduce(merge, swing_permute[[1]]))
+      # returns ordered dataframe; order names and merge
+      swing_permute_names <- swing_names[order(swing_names)]
+      swing_permute <- data.frame("kinase" = swing_permute_names,
+                                  swing_permute)
+      colnames(swing_permute) <- c("kinase", names(n_permute))
       
       if (verbose) {
         message("[Step3/3] : Calculating p-values")
       }
-      #obtain p-values two sided, test independently...
+      #obtain p-values, two sided
       swing_out$p_greater <-
         unlist(bplapply(seq_len(nrow(swing_out)), function(i)
           (sum(
@@ -207,6 +215,8 @@ swing <-
                          ), na.rm = TRUE) + 1)
                          / (as.numeric(permutations) + 1)))
     }
+    # order by p-value
+    swing_out <- swing_out[order(swing_out$p_greater),]
     
     if (verbose) {
       message("[FINISHED]\n")
@@ -214,21 +224,22 @@ swing <-
       message("Finish: ", Sys.time())
     }
     
-    #5. return interaction table - long
-    network <- data_merge
-    #convert to long table:
-    network <- data.table::melt(network,
-                               id.vars = c("annotation"),
-                               measure.vars = 
-                               colnames(network[7:ncol(network)]))
-    # keep "significant" edges
-    network <- network[network$value == 1, ]
-    network <- data.frame(
-      "source" = as.character(network$variable),
-      "target" = as.character(network$annotation)
-    )
-    return(list("scores" = swing_out, "network" = network))
-  }
+    # set option to return a network
+    network <- NULL
+    if (return_network == "TRUE"){
+      network <- data.table::melt(data_merge,
+                                 id.vars = c("annotation"),
+                                 measure.vars = 
+                                 colnames(network[7:ncol(network)]))
+      # keep "significant" edges
+      network <- network[network$value == 1, ]
+      network <- data.frame(
+        "source" = as.character(network$variable),
+        "target" = as.character(network$annotation)
+      )
+    }
+return(list("scores" = swing_out, "network" = network))
+}
 
 # describeIn swing This helper function performs the swing score calculation
 # no verbose in this helper
@@ -241,53 +252,45 @@ swingScore <-
            permute = FALSE,
            n = 1) {
     #----------------------------------------------
-    #count significant positive
-    p_k <-
-      sapply(7:ncol(data_merge), function (i)
-        sum(data_merge$Sipk * data_merge[i] == 1))
-    #count significant negative
-    n_k <-
-      sapply(7:ncol(data_merge), function (i)
-        sum(data_merge$Sipk * data_merge[i] == -1))
+    data_merge <- data_merge$Sipk * data_merge[7:ncol(data_merge)]
+    # propogation of NaN here due to small number division, add psuedo-count
+    # count significant positive
+    p_k <- colSums(data_merge == 1, na.rm = TRUE) + pseudo_count
+    # count significant negative
+    n_k <- colSums(data_merge == -1, na.rm = TRUE) + pseudo_count
     #all counts (both positive and negative).
     t_k <- p_k + n_k
     #----------------------------------------------
-    #compute swing statistics:
+    # compute swing statistics
+    pk_ratio <- (p_k)/(t_k)
+    nk_ratio <- (n_k)/(t_k)
+    swing_raw <- (pk_ratio) / (nk_ratio)
     p_n_all <-
       data.frame(
-        "kinase" = colnames(data_merge[7:ncol(data_merge)]),
+        "kinase" = colnames(data_merge),
         "pos" = p_k,
         "neg" = n_k,
-        "all" = t_k
+        "all" = t_k,
+        "pk" = pk_ratio,
+        "nk" = nk_ratio,
+        "swing_raw" = swing_raw
       )
-    #compute stats:
-    #proportion positive/negative:
-    p_n_all$pk <- p_n_all$pos / p_n_all$all
-    p_n_all$nk <- p_n_all$neg / p_n_all$all
+    # include the count numbers:
+    p_n_all <- merge(p_n_all, pwm_in$kinase, by = "kinase", sort = TRUE)
+    # weighted by number of substrates
+    # not possible to have zero - must have substrates to build model
     p_n_all$swing_raw <-
-      (p_n_all$pk + pseudo_count) / (p_n_all$nk + pseudo_count)
-    #include the count numbers:
-    p_n_all <- merge(p_n_all, pwm_in$kinase, by = "kinase")
-    #weighted by number of substrates:
+      log2(p_n_all$swing_raw) * log2(p_n_all$n)
+    # weighted by size of kinase-substrate network:
     p_n_all$swing_raw <-
-      log2(p_n_all$swing_raw) * log2(p_n_all$n + pseudo_count)
-    #weighted by size of kinase-substrate network:
-    p_n_all$swing_raw <-
-      p_n_all$swing_raw * log2(p_n_all$all + pseudo_count)
-    #z-score transform
+      p_n_all$swing_raw * log2(p_n_all$all)
+    # z-score transform
     p_n_all$swing <-
       (p_n_all$swing_raw - mean(p_n_all$swing_raw, na.rm = TRUE)) / 
       sd(p_n_all$swing_raw, na.rm = TRUE)
-    #order results and return table::
-    p_n_all <- p_n_all[order(p_n_all$swing, decreasing = TRUE),]
-    
-    # for permuted tables, return the scores in
-    # data.table format for merge function!
+
     if (permute == "TRUE") {
-      p_n_all <- data.frame(p_n_all$kinase, p_n_all$swing_raw)
-      colnames(p_n_all) <- c("kinase", paste("swing_raw_", n, sep = ""))
-      p_n_all = data.table(p_n_all, key = 'kinase')
+      p_n_all <- p_n_all$swing_raw
     }
-    
-    return(p_n_all)
-  }
+return(p_n_all)
+}
